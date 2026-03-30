260c161f9d37ba6ca7896dceda76439712f17a05
chohs4164 <josh100as@gmail.com>
2026-03-30 16:12:39 +0900

    chore: 프리뷰 배경색 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/ui/modal/ModalInputTitle.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/ui/modal/ModalInputTitle.kt
@@ -1,10 +1,12 @@
 package woowacourse.kanban.board.task.ui.modal
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,10 +28,10 @@ fun ModalInputTitle(title: String, modifier: Modifier = Modifier) {
 @Composable
 private fun ModalInputTitlePreview() {
     Column(
-        modifier = Modifier.padding(5.dp),
+        modifier = Modifier
+            .background(Color.White)
+            .padding(5.dp),
     ) {
         ModalInputTitle(stringResource(Res.string.label_title))
-
-        ModalInputTitle(stringResource(Res.string.label_description))
     }
 }
