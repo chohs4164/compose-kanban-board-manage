f01f8170b7736bf48988f31e5431285953bd92c7
chohs4164 <josh100as@gmail.com>
2026-04-01 15:45:13 +0900

    refactor: 수업 실습 내용 수정

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/study/SequenceTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/study/SequenceTest.kt
@@ -1,6 +1,11 @@
 package woowacourse.kanban.board.study
 
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
 class SequenceTest {
+    @OptIn(ExperimentalTestApi::class)
     @Test
     fun sequence1() = runComposeUiTest {
         // given
