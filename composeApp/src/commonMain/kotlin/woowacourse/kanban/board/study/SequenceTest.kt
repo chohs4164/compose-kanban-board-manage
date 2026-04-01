91a9a06e7d30bc59395e6006b9d70c961617ef36
chohs4164 <josh100as@gmail.com>
2026-04-01 15:39:31 +0900

    feat: 수업 실습 사항

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/study/SequenceTest.kt
@@ -0,0 +1,11 @@
+package woowacourse.kanban.board.study
+
+class SequenceTest {
+    @Test
+    fun sequence1() = runComposeUiTest {
+        // given
+        val numbers = listOf<Int>()
+        // when
+        // then
+    }
+}
