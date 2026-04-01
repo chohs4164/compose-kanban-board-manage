c8fb60deac76028b710c7a5e7eb6e7fa87566f06
chohs4164 <josh100as@gmail.com>
2026-04-01 18:41:25 +0900

    refactor: ModalSelector Layout LazyRow로 변경하여 가로 스크롤 기능 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/ui/modal/ModalBody.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/ui/modal/ModalBody.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
