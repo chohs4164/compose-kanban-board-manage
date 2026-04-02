e31135ee5add4b20e18750f48d54ce94b5ef35de
chohs4164 <josh100as@gmail.com>
2026-04-03 00:46:39 +0900

    feat: KanbanCard 수정 및 삭제 로직 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/domain/KanbanProject.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/task/domain/KanbanProject.kt
@@ -11,6 +11,21 @@ data class KanbanProject(val projectTitle: String, val kanbanCards: List<KanbanC
 
     fun addCard(kanbanCard: KanbanCard) = copy(kanbanCards = kanbanCards + kanbanCard)
 
+    // 카드의 내용을 수정
+    fun updateCard(id: Long, form: KanbanCardForm, status: KanbanStatus): KanbanProject {
+        val updateCards = kanbanCards.map { card ->
+            if (card.id == id) {
+                card.update(form, status)
+            } else {
+                card
+            }
+        }
+        return copy(kanbanCards = updateCards)
+    }
+
+    // 카드를 삭제
+    fun deleteCard(id: Long): KanbanProject = copy(kanbanCards = kanbanCards.filterNot { it.id == id })
+
     fun getKanbanCard(id: Long): KanbanCard {
         val findKanbanCard = kanbanCards.find { it.id == id } ?: throw IllegalArgumentException("id가 ${id}인 카드를 찾지 못했습니다.")
         return findKanbanCard
